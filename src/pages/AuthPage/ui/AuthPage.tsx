355327013a705ec128430ab922a47d30d0f63b1e
Dmitry Shneider <dmitryshneider@gmail.com>
2024-04-11 00:45:07 +0600

    Тренировка с React-hook-form, заготовка на будущее

--- src/pages/AuthPage/ui/AuthPage.tsx
+++ src/pages/AuthPage/ui/AuthPage.tsx
@@ -1,22 +1,37 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { authorize } from "../../../app/store/authSlice";
-import { useAppDispatch } from "../../../shared/utils/hooks/reduxHooks"
+import { useAppDispatch } from "../../../shared/utils/hooks/reduxHooks";
+import Input from "../../../shared/UiKit/Input";
+import { FormEvent, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 function AuthPage() {
   const dispatch = useAppDispatch();
   const location = useLocation();
   const navigate = useNavigate();
-  const fromPage = location?.state?.from ? location.state.from.pathname : '/auth'
+  const fromPage = location?.state?.from ? location.state.from.pathname : '/auth';
 
-  const handleAuth = () => {
-    dispatch(authorize())
-    navigate(fromPage, {replace: true})
+  interface FormInterface {
+    email: string;
+    password: string;
   }
 
+  const {register, handleSubmit, watch, formState: { errors }} = useForm<FormInterface>();
+  const onSubmit: SubmitHandler<FormInterface> = (data) => console.log(data);
+
+  
+
   return (
-    <div>
+    <div className="container">
       <h1>Авторизация</h1>
-      <button onClick={() => handleAuth()}>Авторизоваться</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" {...register('email', {required: true})}/>
+        {errors.email && <span style={{color: 'red'}}>Это обязательное поле</span>}
+        <input type="password" {...register('password', {required: true})}/>
+        {errors.email && <span style={{color: 'red'}}>Это обязательное поле</span>}
+        <input type="submit" value={'Нажми'}/>
+      </form>
+      
     </div>
   )
 }
